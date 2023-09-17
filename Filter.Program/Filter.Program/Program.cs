f410ee4564ab6fc93577f7e831fa1be837d58e9e
Florescu Alexandru <florescu.alex579774@gmail.com>
2023-09-17 18:41:33 +0300

    Program exit if no input and output file is given

--- Filter.Program/Filter.Program/Program.cs
+++ Filter.Program/Filter.Program/Program.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace PhotoFilter
 {
     public class Program
     {
+        private const int MaxArgumentSize = 2;
+
         public static void Main(string[] args)
         {
+            if (args?.Length != MaxArgumentSize)
+            {
+                Console.WriteLine("Usage: dotnet run [inputFileImage] [outputFileImage]");
+                return;
+            }
+
+            Console.WriteLine("Hello World!");
         }
     }
 }
